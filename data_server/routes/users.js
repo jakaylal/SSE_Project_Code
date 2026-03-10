b0c92e87acd2cb8f51ea7485fd8ae2a9ec06ca79
Sam <samjwalburg@gmail.com>
2026-03-10 18:06:15 -0400

    Added Sign-up & Log-in Functionality
    Signup and Login Works but with no security. I also cleaned up the HTML files and separated the CSS and Javascript into their own files.

--- data_server/routes/users.js
+++ data_server/routes/users.js
@@ -16,7 +16,7 @@ const imageStorage = multer.diskStorage({
 
 const upload = multer({storage: imageStorage});
 
-
+// Geting all
 router.get('/', async (req,res) => {
     try{
         const users = await User.find()
@@ -27,16 +27,18 @@ router.get('/', async (req,res) => {
 })
 //Getting one
 router.get('/:id', getUser,(req, res) => {
-    res.json(res.User)
+    res.json(res.user)
 })
 
 //creating one
-router.post('/', upload.single('image'),async (req,res) => {
+router.post('/',async (req,res) => {
     const user = new User({
-        name: req.body.name,
-        weight: req.body.weight,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
         email: req.body.email,
-        imagePath: req.file.path
+        phone: req.body.phone,
+        username: req.body.username,
+        password: req.body.password,
     })
     try{
         const newUser = await user.save()
@@ -46,20 +48,37 @@ router.post('/', upload.single('image'),async (req,res) => {
     }
 })
 
+//login verify POST
+router.post('/login', async (req,res) => {
+    try{
+        const{username, password} = req.body;
+
+        const user = await User.findOne({username: username})
+
+        if(user && user.password === password){
+            res.status(200).json({message:"login successful", userId: user._id})
+        }else{
+            res.status(401).json({message:"Invalid username or password"})
+        }
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
 //updating one FIX THIS
 router.patch('/:id', getUser,async(req,res) => {
-    if(req.body.name != null){
-        res.user.name = req.body.name
-    }
-    if(req.body.weight != null && req.body.weight == Number){
-        res.user.weight = req.body.weight
-    }
-    if(req.body.email != null){
-        res.user.email = req.body.email
-    }
     try{
-        const updatedUser = await res.user.save()
-        res.json(updatedUser)
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new: true, runValidators: true}
+        );
+
+        if (!updatedUser){
+            return res.status(404).json({message: 'User not found'})
+        }
+
+        res.json(updatedUser);
     }catch(err){
         res.status(400).json({message:err.message})
     }
